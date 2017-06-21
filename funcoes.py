bf0ac19e0945386a1c2129170d432448b6f07c9c
fangyaouea <fy.eng@uea.edu.br>
2017-06-21 17:46:56 -0400

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -2,7 +2,7 @@ import pygame
 import random
 from pygame.locals import*
 pygame.init()
-janela = pygame.display.set_mode((780, 580))
+window = pygame.display.set_mode((780, 580))
 
 #sistema de cores
 WHITE =(255,255,255)
@@ -12,106 +12,106 @@ GREEN = (36, 217, 46)
 YELLOW=(255,255,0)
 PINK =(236,19,182)
 
-opcao = [(80, 80), (80, 100)]
+option = [(80, 80), (80, 100)]
 
 # fonte de texto
 font=pygame.font.SysFont
 
-title = font("Arial black", 72)
+title2 = font("Arial black", 72)
 font_pattern = font("Impact", 30)
 font_text = font("Times New Roman", 15)
 
 
 # textos que aparecerão na tela#
-titulo = title.render("Snake", 1, (RED))
-jogar = font_pattern.render("Jogar (aperte 'J')", 1, (WHITE))
-sobre = font_pattern.render("Sobre (aperte 'S')", 1, (WHITE))
-instrucao = font_pattern.render("Instruções (aperte 'I')", 1, (WHITE))
-instrucao2 = font_pattern.render("Voltar ao menu (aperte 'V') ", 1, (WHITE))
+title = title2.render("Snake", 1, (RED))
+play = font_pattern.render("Jogar (aperte 'J')", 1, (WHITE))
+credit = font_pattern.render("Sobre (aperte 'S')", 1, (WHITE))
+instruction = font_pattern.render("Instruções (aperte 'I')", 1, (WHITE))
+instruction2 = font_pattern.render("Voltar ao menu (aperte 'V') ", 1, (WHITE))
 #no sobre#
-equipe = font_pattern.render("Equipe de Desenvolvimento:", 1, (WHITE))
-nome1 = font_pattern.render("Fang Yao", 1, (WHITE))
-nome2 = font_pattern.render("Ian Gabriel", 1, (WHITE))
-nome3 = font_pattern.render("Wilbert Luís", 1, (WHITE))
-nome4 = font_pattern.render("Yuri Leandro", 1, (WHITE))
-comando = font_pattern.render("Orientação:", 1, (WHITE))
-orientador = font_pattern.render("Prof.Dr. Jucimar Jr", 1, (WHITE))
+team = font_pattern.render("Equipe de Desenvolvimento:", 1, (WHITE))
+name1 = font_pattern.render("Fang Yao", 1, (WHITE))
+name2 = font_pattern.render("Ian Gabriel", 1, (WHITE))
+name3 = font_pattern.render("Wilbert Luís", 1, (WHITE))
+name4 = font_pattern.render("Yuri Leandro", 1, (WHITE))
+commands = font_pattern.render("Orientação:", 1, (WHITE))
+advisor = font_pattern.render("Prof.Dr. Jucimar Jr", 1, (WHITE))
 
 #no jogar#
-placar = font_text.render("Score: ",1, YELLOW)
-class grafica(object):
+scoreboard = font_text.render("Score: ",1, YELLOW)
+class graphic(object):
     def __init__(self):
         print("e começa o jogo")
         pass
-    def fundo(self):
-        janela.fill(WHITE)
+    def background(self):
+        window.fill(WHITE)
         face = pygame.Surface((780, 580))  # criei uma "máscara' com dimensão menor q a tela
         face.fill(BLUE)
-        janela.blit(face, [10, 10])
+        window.blit(face, [10, 10])
 
-    def titulo(self, opcoes):
-        janela.blit(titulo, (280, 250))
-        janela.blit(jogar, (100, 400))
-        janela.blit(instrucao, (100, 450))
-        janela.blit(sobre, (100, 500))
+    def title (self, opcoes):
+        window.blit(title, (280, 250))
+        window.blit(play, (100, 400))
+        window.blit(instruction, (100, 450))
+        window.blit(credit, (100, 500))
 
         pass
 
-    def instrucao(self):
-        imagem = pygame.image.load('inst.PNG')
-        janela.blit(imagem,[175,110])
+    def instruction(self):
+        image = pygame.image.load('inst.PNG')
+        window.blit(image,[175,110])
 
 def pause(self):
-        imagem = pygame.image.load('pause.PNG')
-        janela.blit(imagem,[175,110])
+        image = pygame.image.load('pause.PNG')
+        window.blit(image,[175,110])
 
-    def sobre(self):
-        janela.blit(equipe, (30, 70))
-        janela.blit(nome1, (50, 100))
-        janela.blit(nome2, (50, 150))
-        janela.blit(nome3, (50, 200))
-        janela.blit(nome4, (50, 250))
-        janela.blit(comando, (30, 300))
-        janela.blit(orientador, (50, 350))
-        janela.blit(instrucao2, (30, 550))
+    def credit(self):
+        window.blit(team, (30, 70))
+        window.blit(name1, (50, 100))
+        window.blit(name2, (50, 150))
+        window.blit(name3, (50, 200))
+        window.blit(name4, (50, 250))
+        window.blit(commands, (30, 300))
+        window.blit(advisor, (50, 350))
+        window.blit(instruction2, (30, 550))
 
 
-    def cobra(self, orientacao, comidinha, pontuacao):
-        for i in opcao:
-            pygame.draw.rect(janela,GREEN, (i[0]+1, i[1]+1, 18 , 18))
-            if opcao.index(i) == len(opcao)-1:
-                if orientacao == 0:
-                    opcao.append((i[0] + 20, i[1]))
-                if orientacao == 90:
-                    opcao.append((i[0],i[1]-20))
-                if orientacao == 270:
-                    opcao.append((i[0], i[1]+20))
-                if orientacao == 180:
-                    opcao.append((i[0] - 20, i[1]))
-                if i != comidinha:
-                    del opcao[0]
-                    return (comidinha, pontuacao)
+    def snake(self, orientation, item, punctuation):
+        for i in option:
+            pygame.draw.rect(window,GREEN, (i[0]+1, i[1]+1, 18 , 18))
+            if option.index(i) == len(option)-1:
+                if orientation  == 0:
+                    option.append((i[0] + 20, i[1]))
+                if orientation == 90:
+                    option.append((i[0],i[1]-20))
+                if orientation == 270:
+                    option.append((i[0], i[1]+20))
+                if orientation == 180:
+                    option.append((i[0] - 20, i[1]))
+                if i != item:
+                    del option[0]
+                    return (item, punctuation)
                 else:
-                    pontuacao += 1
-                    return ((random.randint(0, 39)*20, random.randint(0, 29)*20), pontuacao)
+                    punctuation += 1
+                    return ((random.randint(0, 39)*20, random.randint(0, 29)*20), punctuation)
                 break
 
-    def comida(self, comidinha):
-        pygame.draw.rect(janela, RED, (comidinha[0], comidinha[1], 18, 18))
+    def food(self, item):
+        pygame.draw.rect(window, RED, (item[0], item[1], 18, 18))
 
-    def placar(self, pontuacao):
-        janela.blit(placar, (10, 10))
-        pontos = font_text.render(str(pontuacao), 1, PINK)
-        janela.blit(pontos, (50, 30))
+    def scoreboard(self, punctuation):
+        window.blit(scoreboard, (10, 10))
+        point = font_text.render(str(punctuation), 1, PINK)
+        window.blit(point, (50, 30))
 
-    def parede(self):
-        alto=pygame.Rect(0,0,1,600)
-        alto2=pygame.Rect(799,0,1,600)
-        largo=pygame.Rect(0,0,799,1)
-        largo2=pygame.Rect(0,599,800,1)
-        pygame.draw.rect(janela, RED,alto )
-        pygame.draw.rect(janela, RED,alto2 )
-        pygame.draw.rect(janela, RED,largo )
-        pygame.draw.rect(janela, RED, largo2)
+    def wall(self):
+        high=pygame.Rect(0,0,1,600)
+        high2=pygame.Rect(799,0,1,600)
+        large=pygame.Rect(0,0,799,1)
+        large=pygame.Rect(0,599,800,1)
+        pygame.draw.rect(window, RED,high )
+        pygame.draw.rect(window, RED,high2 )
+        pygame.draw.rect(window, RED,large )
+        pygame.draw.rect(window, RED, large2)
 
     
